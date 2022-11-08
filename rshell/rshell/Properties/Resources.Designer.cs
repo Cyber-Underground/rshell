bf961efc8cea6498e5df907d817e163c8148c27f
Spuqe <CryT0r@protonmail.com>
2022-11-08 17:56:06 +0200

    Add files via upload

--- rshell/rshell/Properties/Resources.Designer.cs
+++ rshell/rshell/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CryingWolf.Properties {
+namespace rshell.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CryingWolf.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CryingWolf.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("rshell.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
